1d5e2bcad24420593d803efe94148cc8171a1d62
Oleg Kasian <oleg.kasian@gmail.com>
2025-05-27 16:32:54 +0300

    Create ArticleResponse DTO for Java migration

---
+++ TARGET/src/main/java/realworld/com/articles/dto/ArticleResponse.java
@@ -0,0 +1,14 @@
+package realworld.com.articles.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArticleResponse {
+    private ArticleDto article;
+}
