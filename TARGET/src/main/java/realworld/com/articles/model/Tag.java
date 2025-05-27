1c8659e2fb676bdd4dae466ade03ee69afb20bc0
Oleg Kasian <oleg.kasian@gmail.com>
2025-05-27 16:53:20 +0300

    Add Tag entity for JPA

---
+++ TARGET/src/main/java/realworld/com/articles/model/Tag.java
@@ -0,0 +1,42 @@
+package realworld.com.articles.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tags")
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Tag() {
+    }
+
+    public Tag(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static Tag create(String tagName) {
+        return new Tag(-1L, tagName);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
