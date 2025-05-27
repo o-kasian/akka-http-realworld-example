340bd8ea69e3a036f21a38fefe347328d4bd69ab
Oleg Kasian <oleg.kasian@gmail.com>
2025-05-27 16:32:05 +0300

    Create Favorite model class for Java migration

---
+++ TARGET/src/main/java/realworld/com/articles/model/Favorite.java
@@ -0,0 +1,25 @@
+package realworld.com.articles.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "favorites")
+public class Favorite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "user_id")
+    private Long userId;
+    
+    @Column(name = "favorited_id")
+    private Long favoritedId;
+}
