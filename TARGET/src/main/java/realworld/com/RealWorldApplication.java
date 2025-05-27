73ea1e5963c77ca4845b90a23172a8387b93b812
Oleg Kasian <oleg.kasian@gmail.com>
2025-05-27 17:29:10 +0300

    Create Spring Boot main application class

---
+++ TARGET/src/main/java/realworld/com/RealWorldApplication.java
@@ -0,0 +1,14 @@
+package realworld.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class RealWorldApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(RealWorldApplication.class, args);
+    }
+}
