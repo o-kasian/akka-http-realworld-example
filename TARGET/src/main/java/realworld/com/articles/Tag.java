9144b92ab95be19ba0878bad1948ac0127dd3470
Oleg Kasian <oleg.kasian@gmail.com>
2025-05-27 17:27:28 +0300

    Create Tag JPA entity

---
+++ TARGET/src/main/java/realworld/com/articles/Tag.java
@@ -0,0 +1,75 @@
+package realworld.com.articles;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tags")
+public class Tag {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, unique = true)
+    private String name;
+    
+    @ManyToMany(mappedBy = "tags")
+    private Set<Article> articles = new HashSet<>();
+    
+    // Default constructor required by JPA
+    public Tag() {
+    }
+    
+    public Tag(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    public static Tag create(String tagName) {
+        Tag tag = new Tag();
+        tag.setName(tagName);
+        return tag;
+    }
+    
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Set<Article> getArticles() {
+        return articles;
+    }
+    
+    public void setArticles(Set<Article> articles) {
+        this.articles = articles;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        
+        Tag tag = (Tag) o;
+        
+        return name != null ? name.equals(tag.name) : tag.name == null;
+    }
+    
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+}
