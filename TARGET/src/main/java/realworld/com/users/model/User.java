60f24d459ac5ae7756cd2a81b4b761d84599e931
Oleg Kasian <oleg.kasian@gmail.com>
2025-05-27 16:35:31 +0300

    Create User model class for Java migration

---
+++ TARGET/src/main/java/realworld/com/users/model/User.java
@@ -0,0 +1,31 @@
+package realworld.com.users.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(unique = true)
+    private String username;
+    
+    @Column(unique = true)
+    private String email;
+    
+    private String password;
+    
+    private String bio;
+    
+    private String image;
+}
