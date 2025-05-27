18074c71cbe44498b90051ca539a122cbfe3195c
Oleg Kasian <oleg.kasian@gmail.com>
2025-05-27 16:28:51 +0300

    Migrate User model to Java with JPA annotations

---
+++ src/main/java/realworld/com/users/model/User.java
@@ -0,0 +1,109 @@
+package realworld.com.users.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "username cannot be empty")
+    @Column(unique = true)
+    private String username;
+
+    @NotEmpty(message = "password cannot be empty")
+    private String password;
+
+    @NotEmpty(message = "email cannot be empty")
+    @Column(unique = true)
+    private String email;
+
+    private String bio;
+
+    private String image;
+
+    private Timestamp createdAt;
+
+    private Timestamp updatedAt;
+
+    public User() {}
+
+    public User(Long id, String username, String password, String email, String bio, String image, Timestamp createdAt, Timestamp updatedAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.bio = bio;
+        this.image = image;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
