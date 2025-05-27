7a50e0e312072c249ef819532897ba6bf830478a
Oleg Kasian <oleg.kasian@gmail.com>
2025-05-27 17:09:41 +0300

    Add Tag JPA entity

---
+++ TARGET/src/main/java/realworld/com/model/Tag.java
@@ -0,0 +1,72 @@
+package realworld.com.model;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tags")
+public class Tag {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, unique = true)
+    private String name;
+    
+    @ManyToMany(mappedBy = "tags")
+    private Set<Article> articles = new HashSet<>();
+    
+    // Constructors
+    
+    public Tag() {
+    }
+    
+    public Tag(String name) {
+        this.name = name;
+    }
+    
+    // Getters and Setters
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Set<Article> getArticles() {
+        return articles;
+    }
+    
+    public void setArticles(Set<Article> articles) {
+        this.articles = articles;
+    }
+    
+    // Equals and HashCode
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        
+        Tag tag = (Tag) o;
+        
+        return name != null ? name.equals(tag.name) : tag.name == null;
+    }
+    
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+}
